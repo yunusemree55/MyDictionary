e410bc68a568e81b6f0e2d44352a00279850036f
Yunus Emre Çiçek <ye1374121@gmail.com>
2021-01-22 18:17:25 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<S1, S2>
+    {
+        S1[] _key;
+        S2[] _value;
+        S1[] _tempKey;
+        S2[] _tempValue;
+
+
+        public MyDictionary()
+        {
+            _key = new S1[0];
+            _value = new S2[0];
+        }
+
+        public void Add(S1 item1, S2 item2)
+        {
+            _tempKey = _key;
+            _tempValue = _value;
+            _key = new S1[_key.Length + 1];
+            _value = new S2[_value.Length + 1];
+            for (int i = 0; i < _tempKey.Length; i++)
+            {
+                _key[i] = _tempKey[i];
+            }
+            for (int i = 0; i < _tempValue.Length; i++)
+            {
+                _value[i] = _tempValue[i];
+            }
+            _key[_key.Length - 1] = item1;
+            _value[_value.Length - 1] = item2;
+        }
+
+
+        public int Count
+        {
+            get { return _key.Length; }
+        }
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Dictionary<int, string> sehirler = new Dictionary<int, string>();
+
+            sehirler.Add(0, "Ankara");
+            sehirler.Add(1, "İstanbul");
+
+            Console.WriteLine(sehirler.Count);
+
+
+
+
+            MyDictionary<int, string> isimler = new MyDictionary<int, string>();
+
+            isimler.Add(0, "Yunus Emre");
+            isimler.Add(1, "Furkan");
+            isimler.Add(2, "Mert Ali");
+            isimler.Add(3, "Hasan");
+
+            Console.WriteLine(isimler.Count);
+
+
+
+        }
+    }
+}
