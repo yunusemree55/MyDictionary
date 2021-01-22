77e2c093378881e17696780aa5363841251c3ded
Yunus Emre Çiçek <ye1374121@gmail.com>
2021-01-22 18:30:39 +0300

    Gun4_Odev5

--- MyDictionary/MyDictionary.cs
+++ MyDictionary/MyDictionary.cs
@@ -22,16 +22,24 @@ namespace MyDictionary
         {
             _tempKey = _key;
             _tempValue = _value;
+
+
             _key = new S1[_key.Length + 1];
             _value = new S2[_value.Length + 1];
+
+
             for (int i = 0; i < _tempKey.Length; i++)
             {
                 _key[i] = _tempKey[i];
             }
+
+
             for (int i = 0; i < _tempValue.Length; i++)
             {
                 _value[i] = _tempValue[i];
             }
+
+
             _key[_key.Length - 1] = item1;
             _value[_value.Length - 1] = item2;
         }
